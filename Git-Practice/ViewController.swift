465ccfdc98296570fdb5e9c8923a9ef5c19e1186
mmaybei <yebbi2@ewhain.net>
2024-10-23 01:14:15 +0900

    [feat] 기본 UI 구현

--- Git-Practice/ViewController.swift
+++ Git-Practice/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "깃충돌진짜사건"
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "나도 깃 충돌 해결할 줄 몰라 하암 .."
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setStyle()
+        setUI()
+        setLayout()
     }
-
-
+    
+    private func setStyle() {
+        self.view.backgroundColor = .white
+    }
+    
+    private func setUI() {
+        [titleLabel, subtitleLabel].forEach {
+              $0.translatesAutoresizingMaskIntoConstraints = false
+              self.view.addSubview($0)
+            }
+    }
+    
+    private func setLayout() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            subtitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            subtitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
 }
-
